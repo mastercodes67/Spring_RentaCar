f7ee6b6ebda42226474975be32b501584a256990
Batuhan Akpınar <powerbatu12@gmail.com>
2025-11-25 19:40:28 +0300

    Feat: BaseEntity updated with JPA Auditing (timestamps)

--- src/main/java/com/batuhanakpinar/rentacar/RentacarSpringApplication.java
+++ src/main/java/com/batuhanakpinar/rentacar/RentacarSpringApplication.java
@@ -2,8 +2,10 @@ package com.batuhanakpinar.rentacar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // <-- SİHİRLİ ANAHTAR BU
 public class RentacarSpringApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/batuhanakpinar/rentacar/core/entities/BaseEntity.java
+++ src/main/java/com/batuhanakpinar/rentacar/core/entities/BaseEntity.java
@@ -2,6 +2,10 @@ package com.batuhanakpinar.rentacar.core.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Data
 @MappedSuperclass // 1. JPA'ya diyoruz ki: Bu bir tablo değil, diğer tabloların atasıdır.
@@ -10,5 +14,13 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+
+    @Column(name = "createdDate", updatable = false) // İlk oluştuğunda yazılır, güncellenmez.
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Column(name = "updatedDate") // Her güncellemede tarih değişir.
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
 
 }
